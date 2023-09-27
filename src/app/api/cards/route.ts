9217a835ef28443970d91d9b500d3be45142b5b0
DubravinBohdan <dubravin111@gmail.com>
2023-09-27 18:08:02 +0300

    add basic crud to cards collection

--- src/app/api/cards/route.ts
+++ src/app/api/cards/route.ts
@@ -1,25 +1,25 @@
 import { NextResponse } from "next/server";
-import { createCardDto } from "./dto";
+import { createColumnDto } from "./dto";
 import { prisma } from "@/app/core/prisma";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const column_id = searchParams.get("column_id");
-  if (!column_id) {
+  const board_id = searchParams.get("board_id");
+  if (!board_id) {
     return NextResponse.json(
       [
         {
           code: "missing query param",
-          field: "column_id",
-          message: "column_id in query params is required",
+          field: "board_id",
+          message: "board_id in query params is required",
         },
       ],
       { status: 400 }
     );
   }
 
-  const boards = await prisma.cards.findMany({
-    where: { column_id },
+  const boards = await prisma.columns.findMany({
+    where: { board_id },
     orderBy: { order: "asc" },
   });
 
@@ -28,26 +28,27 @@ export async function GET(req: Request) {
 
 export async function POST(req: Request) {
   const body = await req.json();
-  const validateBody = createCardDto.safeParse(body);
+  const validateBody = createColumnDto.safeParse(body);
 
   if (!validateBody.success) {
     return NextResponse.json(validateBody.error.issues, { status: 400 });
   }
 
-  const { title, column_id } = validateBody.data;
+  const { title, width, board_id } = validateBody.data;
 
-  const lastCard = await prisma.cards.findFirst({
-    where: { column_id },
+  const lastColumn = await prisma.columns.findFirst({
+    where: { board_id },
     orderBy: { order: "desc" },
   });
 
-  const newCard = await prisma.cards.create({
+  const newColumn = await prisma.columns.create({
     data: {
       title,
-      column_id,
-      order: lastCard ? lastCard.order + 1 : 0,
+      width,
+      board_id,
+      order: lastColumn ? lastColumn.order + 1 : 0,
     },
   });
 
-  return NextResponse.json(newCard);
+  return NextResponse.json(newColumn);
 }
