4a854c50c30c852e96a8f5aec10c5c99b3d4ae57
DubravinBohdan <dubravin111@gmail.com>
2023-10-19 13:25:22 +0300

    fetch boards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
+import { prisma } from "@/core/prisma";
 import { BoardCard } from "../components/BoardCards";
 import { NavBar } from "../components/NavBar";
 
-export default function Home() {
+export default async function Home() {
+  const boards = await prisma.boards.findMany();
   return (
     <div>
       <NavBar />
       <div className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 2xl:grid-cols-5 px-4 lg:px-12">
-        {Array(5)
-          .fill(0)
-          .map((i, k) => {
-            return <BoardCard id={String(k)} title={`Board ${k}`} />;
-          })}
+        {boards.map((board) => {
+          return <BoardCard id={board.id} title={board.title} />;
+        })}
       </div>
     </div>
   );
